d782a755a386a461bc04c4abfc2c9b36972859d4
Yulia <121833232+NishtaYulia@users.noreply.github.com>
2023-02-04 18:00:00 +0200

    add admin

--- bot/src/echobot.py
+++ bot/src/echobot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from .settings import BOT_TOKEN
-#from bot.src.database.create_table import execute_query
+from bot.src.database.create_table import execute_query
 from zoneinfo import ZoneInfo
 from datetime import datetime
 
@@ -13,10 +13,10 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    #timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
-    #insert_query = (f"INSERT INTO message (message, userid, message_time) "
-    #                f"VALUES ({message.text}, {message.from_user['id']}, {timestamp_now})")
-    #execute_query(insert_query)
+    timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
+    insert_query = (f"INSERT INTO message (message, userid, message_time) "
+                    f"VALUES ({message.text}, {message.from_user['id']}, {timestamp_now})")
+    execute_query(insert_query)
     await message.reply("Hi!\nI'm EchoBot!")
 
 
@@ -25,8 +25,8 @@ async def echo(message: types.Message):
     """
     This handler accepts any message and sends it back unchanged.
     """
-    #timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
-    #insert_query = (f"INSERT INTO message (message, userid, message_time) "
-    #                f"VALUES ('{message.text}', {message.from_user['id']}, '{timestamp_now}')")
-    #execute_query(insert_query)
+    timestamp_now = datetime.now(tz=ZoneInfo("UTC")).isoformat(" ")
+    insert_query = (f"INSERT INTO message (message, userid, message_time) "
+                    f"VALUES ('{message.text}', {message.from_user['id']}, '{timestamp_now}')")
+    execute_query(insert_query)
     await message.answer(message.text)
